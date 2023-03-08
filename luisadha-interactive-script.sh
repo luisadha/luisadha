ed1dcaaf422236604a1012b528d42b7c1d7255f5
Luis Adha <40796508+luisadha@users.noreply.github.com>
2023-03-09 03:22:03 +0700

    Add files via upload

---
+++ luisadha-interactive-script.sh
@@ -0,0 +1,34 @@
+cd script/interactive
+
+PS3="Your choice: "
+
+files="$(ls -Art .)"
+
+echo "------------------------------"
+echo " Luisadha-interactive-script"
+echo " v1.0"
+echo "------------------------------"
+echo -e "Provide some tools from repository:\n Visit https://github.com/luisadha/luisadha"
+echo
+
+select filename in ${files} Quit;
+do
+[[ -n $filename ]] || { echo "What's that? Please try again." >&2; continue; }
+ 
+  case ${filename} in
+    "Quit")
+      echo "Exiting.."
+      break
+      ;;
+    *)
+    	echo "You selected $filename ($REPLY)"
+    echo
+      chmod +x ${filename}/${filename,,}.sh
+      ./${filename}/${filename,,}.sh
+    echo
+     read -t 0.1
+     continue
+     ;;
+  esac
+done
+
