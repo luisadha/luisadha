db38767e2a0dbf338be98ca0dc0408139baee0cd
Luis Adha <40796508+luisadha@users.noreply.github.com>
2023-07-27 12:59:52 +0700

    Delete luisadha-source-script.sh

--- luisadha-source-script.sh
+++
@@ -1,9 +0,0 @@
-for p in ~/luisadha/script/executeable; do
-        [[ -d $p/. ]] || continue
-        [[ :$PATH: = *:$p:* ]] || PATH=$p:$PATH
-done
-
-unset p
-
-echo "Script below already to call from terminal"
-ls ~/luisadha/script/executeable;
